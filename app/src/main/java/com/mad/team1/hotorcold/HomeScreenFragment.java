c651c6c4f3f0776bd6a2731f48be6048828bdabc
GurinderSingh <g.singh-13@student.lboro.ac.uk>
2016-04-27 16:11:55 +0100

    Added Start Game Fragment and code to for onClick

--- app/src/main/java/com/mad/team1/hotorcold/HomeScreenFragment.java
+++ app/src/main/java/com/mad/team1/hotorcold/HomeScreenFragment.java
@@ -1,24 +1,48 @@
 package com.mad.team1.hotorcold;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 
 /**
  * Created by GurinderSingh on 27/04/2016.
  */
 
-public class HomeScreenFragment extends Fragment {
+public class HomeScreenFragment extends Fragment implements View.OnClickListener {
+
+    Button myButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home_screen_layout, container, false);
+        View myView = inflater.inflate(R.layout.home_screen_layout, container, false);
+        myButton = (Button) myView.findViewById(R.id.start_button);
+        myButton.setOnClickListener(this);
+        return myView;
     }
+    @Override
+    public void onClick(View v) {
+        // Create new fragment and transaction
+        Fragment newFragment = new StartGameFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack
+        transaction.replace(R.id.home_screen_fragment, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+
+    }
+
 
 
 }
---
+++ app/src/main/java/com/mad/team1/hotorcold/StartGameFragment.java
@@ -0,0 +1,19 @@
+package com.mad.team1.hotorcold;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by GurinderSingh on 27/04/2016.
+ */
+public class StartGameFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.start_game_screen, container, false);
+    }
+}
