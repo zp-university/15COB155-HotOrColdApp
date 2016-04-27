3bd73ffa42dff2a680c1bab24a23f697de234e4f
GurinderSingh <g.singh-13@student.lboro.ac.uk>
2016-04-27 23:49:06 +0100

    Linked StartGameFragment startGameButton to inGameFragment

--- app/src/main/java/com/mad/team1/hotorcold/InGameFragment.java
+++ app/src/main/java/com/mad/team1/hotorcold/InGameFragment.java
@@ -1,15 +1,20 @@
 package com.mad.team1.hotorcold;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
-public class InGameFragment extends PreferenceFragment {
+public class InGameFragment extends Fragment{
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.in_game_screen, container, false);
     }
 
-
 }
+
