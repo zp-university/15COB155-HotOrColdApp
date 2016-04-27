5c08194f6b6af41fd2102592edb10a107e2f6a31
Matt Weaver <m.weaver-14@student.lboro.ac.uk>
2016-04-27 16:54:00 +0100

    Fragment screwed up

--- app/src/main/java/com/mad/team1/hotorcold/HomeScreenFragment.java
+++ app/src/main/java/com/mad/team1/hotorcold/HomeScreenFragment.java
@@ -3,46 +3,54 @@ package com.mad.team1.hotorcold;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 /**
  * Created by GurinderSingh on 27/04/2016.
  */
 
-public class HomeScreenFragment extends Fragment implements View.OnClickListener {
-
-    Button myButton;
+public class HomeScreenFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View myView = inflater.inflate(R.layout.home_screen_layout, container, false);
-        myButton = (Button) myView.findViewById(R.id.start_button);
+        Button myButton = (Button) myView.findViewById(R.id.start_button);
         myButton.setOnClickListener(this);
         return myView;
     }
+
     @Override
     public void onClick(View v) {
-        // Create new fragment and transaction
-        Fragment newFragment = new StartGameFragment();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        //do what you want to do when button is clicked
+        switch (v.getId()) {
+            case R.id.start_button:
+                //switchFragment(HelpFragment.TAG);
 
-        // Replace whatever is in the fragment_container view with this fragment,
-        // and add the transaction to the back stack
-        transaction.replace(R.id.home_screen_fragment, newFragment);
-        transaction.addToBackStack(null);
+                Fragment newFragment = new StartGameFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
 
-        // Commit the transaction
-        transaction.commit();
+                // Replace whatever is in the fragment_container view with this fragment,
+                // and add the transaction to the back stack
+                transaction.replace(R.id.home_screen_fragment, newFragment);
+                transaction.addToBackStack(null);
 
+                // Commit the transaction
+                transaction.commit();
+
+                break;
+        }
     }
-
 
 
 }
--- app/src/main/java/com/mad/team1/hotorcold/MainActivity.java
+++ app/src/main/java/com/mad/team1/hotorcold/MainActivity.java
@@ -1,14 +1,76 @@
 package com.mad.team1.hotorcold;
 
+import android.app.FragmentManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.FragmentTransaction;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        HomeScreenFragment homeFragment = new HomeScreenFragment();
+        fragmentTransaction.add(R.id.home_screen_layout, homeFragment);
+        fragmentTransaction.commit();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.mad.team1.hotorcold/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.mad.team1.hotorcold/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
