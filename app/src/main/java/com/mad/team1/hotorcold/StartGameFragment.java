3e6752fd2b3a067db86e74f9b9b6b136daebc993
Matt Weaver <m.weaver-14@student.lboro.ac.uk>
2016-04-27 18:48:12 +0100

    StartGame seekbar added

--- app/src/main/java/com/mad/team1/hotorcold/StartGameFragment.java
+++ app/src/main/java/com/mad/team1/hotorcold/StartGameFragment.java
@@ -5,15 +5,62 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by GurinderSingh on 27/04/2016.
  */
 public class StartGameFragment extends Fragment {
 
+    private SeekBar seekBar;
+    private TextView textView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.start_game_screen, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        seekBar = (SeekBar) view.findViewById(R.id.seekBar);
+        textView = (TextView) view.findViewById(R.id.seekBarText);
+
+        textView.setText("Covered: " + seekBar.getProgress() + "/" + seekBar.getMax());
+        if( savedInstanceState != null ) {
+            textView.setText("Covered: " + savedInstanceState.getInt("seekBarProgress") + "/" + seekBar.getMax());
+        }
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = seekBar.getProgress();
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                Toast.makeText(getActivity().getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getActivity().getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textView.setText("Covered: " + progress + "/" + seekBar.getMax());
+                Toast.makeText(getActivity().getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("seekBarProgress", seekBar.getProgress());
+    }
+
 }
