2c54a64e6e96503c3522bbdce8da04c600daadc3
GurinderSingh <g.singh-13@student.lboro.ac.uk>
2016-04-27 23:01:49 +0100

    Added InGame Fragment ann circle_shape.xml

---
+++ app/src/main/java/com/mad/team1/hotorcold/InGameFragment.java
@@ -0,0 +1,15 @@
+package com.mad.team1.hotorcold;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+
+public class InGameFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
