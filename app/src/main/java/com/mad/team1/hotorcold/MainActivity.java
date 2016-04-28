fe991b42d7f4268031908fb3ac68688fd4d6c28f
GurinderSingh <g.singh-13@student.lboro.ac.uk>
2016-04-28 01:03:41 +0100

    Added Battery Low Toast - Does not work

--- app/src/main/java/com/mad/team1/hotorcold/MainActivity.java
+++ app/src/main/java/com/mad/team1/hotorcold/MainActivity.java
@@ -1,12 +1,14 @@
 package com.mad.team1.hotorcold;
 
 import android.app.FragmentManager;
-import android.content.SharedPreferences;
-import android.net.Uri;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.FragmentTransaction;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity  {
@@ -33,6 +35,22 @@ public class MainActivity extends AppCompatActivity  {
         } else {
             getFragmentManager().popBackStack();
         }
+    }
+
+    public class MyBatteryLevelReceiver extends BroadcastReceiver
+
+    {
+
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            if(intent.getAction().equals(Intent.ACTION_BATTERY_LOW))
+            {
+                Toast.makeText(context,R.string.low_battery_message, Toast.LENGTH_LONG).show();
+                // do your task here.
+            }
+        }
+
     }
 
 }
